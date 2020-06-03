489076e1be440e22b0e3bf3199877a52ca06cb87
Rincevent42 <porvooc@gmail.com>
2020-06-03 11:49:02 +0200

    implementation of react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import Listing from './pages/Listing';
+import Registration from './pages/Registration';
+import Generic from './pages/Generic';
+import SearchResult from './pages/Research-result';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       Les Freelances Lyonnais - Front office
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/detail'>
+            <Detail />
+          </Route>
+          <Route path='/listing'>
+            <Listing />
+          </Route>
+          <Route path='/registration'>
+            <Registration />
+          </Route>
+          <Route path='/generic'>
+            <Generic />
+          </Route>
+          <Route path='/search-results'>
+            <SearchResult />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
