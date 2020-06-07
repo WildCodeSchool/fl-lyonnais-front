c0d5bee9c365ed090148ced5e60d6477269c50ca
Pierre <pierre.genthon@wildcodeschool.fr>
2020-06-07 13:30:50 +0200

    Sprint 1 (#19)
    * implementation of react router
    
    * push gitignore
    
    * fix problem
    
    * Mise en place des pages et composants primaires
    
    * update
    
    * update
    
    * fix semistandard
    
    * update
    
    * update
    
    * barre de recherche dans SearchBar
    
    * add header
    
    * bug registration
    
    * Ptit commit de fin de journée
    
    * add registration page
    
    * update
    
    * Ajout du footer
    
    * update registration
    
    * #2 Page générique
    
    * add registration page
    
    * update
    
    * update registration
    
    * delete copy of legal_disclaimer
    
    * switch css variables in to App.js
    
    * update
    
    * update registration
    
    * semistandard
    
    Co-authored-by: Rincevent42 <porvooc@gmail.com>
    Co-authored-by: wdwcs <arminit3@gmail.com>

---
+++ src/pages/Listing.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+// import '../styles/Listing.css';
+
+function Listing (props) {
+  return (
+    <div className='Listing'>
+      <Header />
+
+      Nice to meet you !
+
+      <Footer />
+
+    </div>
+  );
+}
+
+export default Listing;
