b728086bd813ca44e08badff17afd8a0feb8855d
ikramb-ctrl <b_zakia@homail.com>
2020-06-10 10:38:12 +0200

    changement des fiches

--- src/components/Freelance.js
+++ src/components/Freelance.js
@@ -1,57 +1,39 @@
 import React from 'react';
-import '../styles/Freelance.css';
+import { Card, Icon, Image } from 'semantic-ui-react';
+//import '../styles/Freelance.css';
+
 
 const Freelance = (props) => {
   const { urlPhoto, firstname, lastname, bio, siret, email, phone } = props.freelance;
 
   return (
-    <div className='freelance'>
-      <figure className='ListingCard'>
 
-        <div className='photo'>
-          <img src={urlPhoto} alt={firstname} />
-          {firstname} {lastname}
-        </div>
+  <Card>
+    <Image src={urlPhoto} wrapped ui={false} />
+    <Card.Content>
+  <Card.Header>{firstname}{lastname}</Card.Header>
+      <Card.Meta>
+        <span className='siret'>Siret: {siret}</span>
+      </Card.Meta>
+      <Card.Description>
+        {bio}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <p>
+        <Icon name='contact' />
+        {email}
+      </p>
+      <p>
+        <Icon name='contact' />
+        {phone}
+      </p>
+    </Card.Content>
+  </Card>
 
-        <figcaption>
+   );
 
-          <div className='dropdown'>
-            <span>Plus d'information</span>
-            <div className='dropdown-content'>
-
-              <strong> Biographie</strong> : {bio}
-
-              <blockquote>
-                <hr />
-                <strong> Siret </strong> : {siret}
-              </blockquote>
-
-              <hr />
-
-            </div>
-          </div>
-
-          <blockquote>
-            <h4> Contact</h4>
-          </blockquote>
-
-          <blockquote>
-            <strong> Numéro de téléphone </strong> : {phone}
-          </blockquote>
-
-          <blockquote>
-            <strong> @ </strong> : {email}
-          </blockquote>
-
-          <button>
-              + Afficher plus d'information
-          </button>
-
-        </figcaption>
-
-      </figure>
-    </div>
-  );
-};
+  
+}
 
 export default Freelance;
