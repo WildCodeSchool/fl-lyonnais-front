8f07afba5b32ac4013a76185810f65d6467b9682
ikramb-ctrl <b_zakia@homail.com>
2020-06-11 15:37:40 +0200

    affichage listing

--- src/components/Freelance.js
+++ src/components/Freelance.js
@@ -1,43 +1,43 @@
 import React from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import { Card, Image } from 'semantic-ui-react';
 import '../styles/Freelance.css';
 
-
 const Freelance = (props) => {
-  const { urlPhoto, firstname, lastname, bio, siret, email, phone } = props.freelance;
+  const { urlPhoto, firstname, lastname, mainSkills } = props.freelance;
 
   return (
-<div className="card">
-  <Card className="freelance">
+    <div className='card'>
+      <Card className='freelance'>
 
-    <Image className="photo" src={urlPhoto} wrapped ui={false} />
-    <Card.Content>
-  <Card.Header className="card-header">{firstname}{lastname}</Card.Header>
-      <Card.Meta>
-        <span className='siret'>Siret: {siret}</span>
-      </Card.Meta>
-      <Card.Description className="card-informations">
-        {bio}
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <p>
-        <Icon name='contact' />
-        {email}
-      </p>
-      <p>
-        <Icon name='contact' />
-        {phone}
-      </p>
-      <button> + Afficher plus d'information </button>
-    </Card.Content>
-  
-  </Card>
-  </div>
+        <Image className='photo' src={urlPhoto} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header className='card-header'>{firstname} {lastname}</Card.Header>
+          <Card.Meta>
+            <span className='mainSkills'>{mainSkills[0]} | {mainSkills[1]} | {mainSkills[2]}</span>
+          </Card.Meta>
+          {/* <Card.Description className="card-informations">
+            {bio}
+          </Card.Description> */}
+        </Card.Content>
+        {/*
+        <Card.Content extra>
+          <p>
+            <Icon name='contact' />
+            {email}
+          </p>
+          <p>
+            <Icon name='contact' />
+            {phone}
+          </p>
+          */}
+          <Card.Content>
+          <button> + Plus d'informations </button>
+        </Card.Content>
+        
+      </Card>
+    </div>
 
-   );
-
-  
-}
+  );
+};
 
 export default Freelance;
