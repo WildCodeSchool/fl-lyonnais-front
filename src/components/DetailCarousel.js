295e2429b93f8dd3bf4a806490ffd93b08865764
florenthoudeille <houdeille.florent@hotmail.fr>
2020-06-11 09:36:56 +0200

    remove carousel library component, begin of style the detail page

--- src/components/DetailCarousel.js
+++ src/components/DetailCarousel.js
@@ -1,47 +1,10 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
 
 class DetailCarousel extends React.Component {
   render () {
     return (
-      <div>
-        <Carousel>
-          <Carousel.Item className='carousel'>
-            <img
-              className='d-block w-100'
-              src='https://cdn.pocket-lint.com/r/s/1200x/assets/images/151442-cameras-feature-stunning-photos-from-the-national-sony-world-photography-awards-2020-image1-evuxphd3mr.jpg'
-              alt='First slide'
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className='d-block w-100'
-              src='https://cdn.pocket-lint.com/r/s/1200x/assets/images/151442-cameras-feature-stunning-photos-from-the-national-sony-world-photography-awards-2020-image1-evuxphd3mr.jpg'
-              alt='Third slide'
-            />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className='d-block w-100'
-              src='https://cdn.pocket-lint.com/r/s/1200x/assets/images/151442-cameras-feature-stunning-photos-from-the-national-sony-world-photography-awards-2020-image1-evuxphd3mr.jpg'
-              alt='Third slide'
-            />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+      <div className=''>
+        
       </div>
     );
   }
