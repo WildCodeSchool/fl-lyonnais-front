e50ded1b3efdd53591a0e0550928559d68813b68
Pascal-Pereira <pereira.pascal@gmail.com>
2020-06-12 09:45:57 +0200

    no verify

--- src/components/DetailReferences.js
+++ src/components/DetailReferences.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import '../styles/DetailReferences.css';
 
-function DetailReferences ({freelance}) {
+function DetailReferences ({ freelance }) {
   return (
     <div>
       <h1>Mes Références</h1>
       {freelance.references.map(reference => {
         return (
           <div className='carousel'>
-            <p>{reference.name}</p> 
-            <img src={reference.image} alt={reference.name}/>
+            <p>{reference.name}</p>
+            <img src={reference.image} alt={reference.name} />
           </div>
-        )
-      })
-      }
+        );
+      })}
     </div>
   );
 }
-
 
 export default DetailReferences;
--- src/components/Reference.js
+++ src/components/Reference.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../styles/Reference.css';
 
-function Reference( {freelance} ) {
+function Reference ({ freelance }) {
   return (
     <div className='carousel'>
       <p className='para '>{freelance.name}</p>
       <img src={freelance.image} alt={freelance.name} />
     </div>
   );
-};
+}
 
 export default Reference;
