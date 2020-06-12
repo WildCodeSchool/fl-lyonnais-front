56f1dd45917666a9584e40aaf07cf983effce0e9
Pascal-Pereira <pereira.pascal@gmail.com>
2020-06-12 09:44:38 +0200

    fusion branche

--- src/components/DetailReferences.js
+++ src/components/DetailReferences.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
-import DetailCarousel from './DetailCarousel';
+import React from 'react';
+import '../styles/DetailReferences.css';
 
-class DetailReferences extends Component {
-  render () {
-    return (
-      <div>
-        <DetailCarousel ModalProps='false' mobile='true' />
-      </div>
-    );
-  }
+function DetailReferences ({freelance}) {
+  return (
+    <div>
+      <h1>Mes Références</h1>
+      {freelance.references.map(reference => {
+        return (
+          <div className='carousel'>
+            <p>{reference.name}</p> 
+            <img src={reference.image} alt={reference.name}/>
+          </div>
+        )
+      })
+      }
+    </div>
+  );
 }
+
 
 export default DetailReferences;
