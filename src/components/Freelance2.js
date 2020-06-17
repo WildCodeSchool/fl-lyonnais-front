cdb1d4d45a9effe826cad8e6f584837a614a90ee
ikramb-ctrl <b_zakia@homail.com>
2020-06-17 18:30:48 +0200

    appel à l'api pour le listing

--- src/components/Freelance2.js
+++ src/components/Freelance2.js
@@ -2,27 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Freelance2.scss';
 
+
 const Freelance = (props) => {
-  const { id, urlPhoto, firstname, lastname, mainSkills } = props.freelance;
 
   return (
     <div className='freelance2'>
       <div className='container'>
         <div className='card'>
-          <Link to={`/detail/${id}`}>
+          <Link to={`/detail/${props.id}`}>
 
             <div className='card-image'>
-              <img src={urlPhoto} alt='Orange' />
+              <img src={props.urlPhoto} alt='Orange' />
             </div>
 
             <div className='card-body'>
 
               <div className='card-name'>
-                <h4>{firstname} {lastname}</h4>
+                <h4>{props.firstname} {props.lastname}</h4>
               </div>
 
               <div className='card-title'>
-                <p>{mainSkills[0]} | {mainSkills[2]} | {mainSkills[2]}</p>
+                <p>{props.job_title}</p>
               </div>
 
             </div>
