adb0d6d483b105fa197ac85c2da256ec83838557
florenthoudeille <houdeille.florent@hotmail.fr>
2020-06-17 16:59:39 +0200

    Fix style of Detail page.

--- src/components/DetailSkills.js
+++ src/components/DetailSkills.js
@@ -6,20 +6,23 @@ function detailSkills (props) {
   return (
     <div>
       <h2 className='detailh2'>Mes compétences</h2>
-      <div className='mainSkills'>
-        <ul>
-          {props.freelance.mainSkills.map(mainSkill => {
-            return (<li key={generateKey(mainSkill)}>{mainSkill}</li>);
-          })}
-        </ul>
+      <div className="detailskills">
+        <div className='mainSkills'>
+          <ul>
+            {props.freelance.mainSkills.map(mainSkill => {
+              return (<li key={generateKey(mainSkill)}>{mainSkill}</li>);
+            })}
+          </ul>
+        </div>
+        <div className='otherSkills'>
+          <ul>
+            {props.freelance.otherSkills.map(otherSkill => {
+              return (<li key={generateKey(otherSkill)}>{otherSkill}</li>);
+            })}
+          </ul>
+        </div>
       </div>
-      <div className='otherSkills'>
-        <ul>
-          {props.freelance.otherSkills.map(otherSkill => {
-            return (<li key={generateKey(otherSkill)}>{otherSkill}</li>);
-          })}
-        </ul>
-      </div>
+      
       <p id='detailskillstjm'>TJM : à partir de <span className="detailskillstjmprice">{props.freelance.average_daily_rate}</span> € par jour.</p>
     </div>
   );
