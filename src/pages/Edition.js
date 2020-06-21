be4310fa1509c712ce0ccf19f3eb41cfc4665d19
Pascal-Pereira <pereira.pascal@gmail.com>
2020-06-21 14:53:37 +0200

    Modal PW

--- src/pages/Edition.js
+++ src/pages/Edition.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup,Label, CustomInput  } from 'reactstrap';
 
 import Form from 'react-bootstrap/Form';
 // import { Button } from 'react-bootstrap';
@@ -8,7 +8,7 @@ import { Col } from 'react-bootstrap';
 import '../styles/Edition.css';
 import '../components/PasswordModal';
 
-function PasswordModal (props) {
+function PasswordModal(props) {
   const {
     buttonLabel,
     className
@@ -21,28 +21,28 @@ function PasswordModal (props) {
   const externalCloseBtn = <button className='close' style={{ position: 'absolute', top: '15px', right: '15px' }} onClick={toggle}>&times;</button>;
   return (
     <div>
-      <Button color='danger' onClick={toggle}>{buttonLabel}</Button>
+      <Button color='primary' onClick={toggle}><h3>Changer de mot de passe</h3></Button>
       <Modal isOpen={modal} toggle={toggle} className={className} external={externalCloseBtn}>
-        <ModalHeader>Modal title</ModalHeader>
+        <ModalHeader>Changement de mot de passe</ModalHeader>
         <ModalBody>
 
           <Form.Group as={Col} controlId='formBasicCurrentPassword'>
             <Form.Label>Mot de passe actuel</Form.Label>
-            <Form.Control type='currentpassword' placeholder='mot de passe actuel' />
+            <Form.Control type='currentpassword' placeholder='********' />
           </Form.Group>
 
           <Form.Group as={Col} controlId='formBasicCurrentPassword'>
             <Form.Label>Nouveau mot de passe</Form.Label>
-            <Form.Control type='newpassword' placeholder='nouveau mot de passe' />
+            <Form.Control type='newpassword' placeholder='' />
           </Form.Group>
 
           <Form.Group as={Col} controlId='formBasicCurrentPassword'>
             <Form.Label>Confirmer mot de passe</Form.Label>
-            <Form.Control type='newpasswordconfirmed' placeholder='confirmer mot de passe' />
+            <Form.Control type='newpasswordconfirmed' placeholder='' />
           </Form.Group>
         </ModalBody>
         <ModalFooter>
-          <Button color='primary' onClick={toggle}>Do Something</Button>{' '}
+          <Button color='primary' onClick={toggle}>Confirmer</Button>{' '}
           <Button color='secondary' onClick={toggle}>Cancel</Button>
         </ModalFooter>
       </Modal>
@@ -50,12 +50,12 @@ function PasswordModal (props) {
   );
 }
 
-function Edition (props) {
+function Edition(props) {
   return (
     <div className='edition'>
       <Form>
         {/* <Form.Row> */}
-        <h2>Vos informations</h2>
+        <h2>Informations</h2>
         <Form.Group as={Col} controlId='formGridfirstname'>
           <Form.Label>Prénom</Form.Label>
           <Form.Control type='prenom' placeholder='John' />
@@ -67,19 +67,19 @@ function Edition (props) {
         </Form.Group>
         {/* </Form.Row> */}
 
-        <h2>Votre photo</h2>
-        <div class='form-group'>
-          <label for='exampleFormControlFile1'>Télécharger votre photo de profil</label>
-          <input type='file' class='form-control-file  text-center' id='exampleFormControlFile1' />
-        </div>
-
-        <h2>Votre biographie</h2>
+        {/* <h2>Photo de profil</h2> */}
+        <FormGroup controlId='formGridpicture' className='profilpic '>
+          <Label  for="exampleCustomFileBrowser">Photo de profil</Label>
+          <CustomInput type="file" id="exampleCustomFileBrowser" name="customFile" />
+        </FormGroup>
+{/* 
+        <h2>Votre biographie</h2> */}
         <Form.Group as={Col} controlId='exampleForm.ControlTextearea1'>
-          <Form.Label>Example textarea</Form.Label>
+          <Form.Label>Biographie</Form.Label>
           <Form.Control as='textarea' rows='3' />
         </Form.Group>
 
-        <h2>Votre références</h2>
+        <h2>Références</h2>
         <Form.Group as={Col} controlId='formGridprojectName'>
           <Form.Label>Nom du projet / client</Form.Label>
           <Form.Control type='projet' placeholder='projet' />
@@ -89,13 +89,13 @@ function Edition (props) {
           <Form.Control type='url_projet' placeholder='url du projet' />
         </Form.Group>
 
-        <h2>Vos compétences</h2>
+        <h2>Compétences</h2>
         <Form.Group as={Col} controlId='formGridAddress1'>
           <Form.Label>Compétences</Form.Label>
           <Form.Control placeholder='compétenes autocomplétion à implémenter' />
         </Form.Group>
 
-        <h2>Votre informations de Freelance</h2>
+        <h2>Informations complémentaires</h2>
         <Form.Group as={Col} controlId='formGridproemail'>
           <Form.Label>Adresse e-mail professionnelle</Form.Label>
           <Form.Control type='emailpro' placeholder='jdoe@doej.com' />
@@ -128,7 +128,7 @@ function Edition (props) {
           <Form.Control type='tva' placeholder='1234567187633eedede' />
         </Form.Group>
 
-        <h2>Changer de mot de passe</h2>
+
         {/*
         <Form.Group as={Col} controlId="formBasicCurrentPassword">
           <Form.Label>Mot de passe actuel</Form.Label>
@@ -148,11 +148,11 @@ function Edition (props) {
           <PasswordModal />
         </Form.Group>
 
-        <Form.Group as={Col} id='formGridCheckbox'>
-          <Form.Check type='checkbox' label='Check me out' />
-        </Form.Group>
+          {/* <Form.Group as={Col} id='formGridCheckbox'>
+            <Form.Check type='checkbox' label='Prise en compte des mentions légales' />
+          </Form.Group>  */}
 
-        <Button variant='primary' type='submit'>
+        <Button color='danger' type='submit'>
           Submit
         </Button>
 
