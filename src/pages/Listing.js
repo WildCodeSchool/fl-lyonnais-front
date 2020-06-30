5dc32193be10e56d9c1dbc0f0b1fe90cafcc1ed5
Armin <arminit3@gmail.com>
2020-06-30 15:22:45 +0200

    add pagination

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -4,8 +4,9 @@ import Freelance from '../components/Freelance2';
 import '../styles/Listing.scss';
 import axios from 'axios';
 import { generateKey } from '../functionshelper';
-//import InfiniteScroll from 'react-infinite-scroll-component';
+// import InfiniteScroll from 'react-infinite-scroll-component';
 import ScrollButton from '../components/ScrollButton';
+import BasicPagination from '../components/Pagination';
 
 const elementsPerPage = 10;
 
@@ -21,10 +22,10 @@ class Listing extends Component {
   fetchData = async () => {
     const newOffSet = this.state.offSet + 1;
     this.setState({ offSet: newOffSet });
-    await this.load(newOffSet * elementsPerPage, (newOffSet + 1) * elementsPerPage)
-    console.log(this.state.dataResults)
+    await this.load(newOffSet * elementsPerPage, (newOffSet + 1) * elementsPerPage);
+    console.log(this.state.dataResults);
   }
-  
+
   componentDidMount () {
     axios
       .get('https://bridge.buddyweb.fr/api/freelancers/test')
@@ -39,17 +40,16 @@ class Listing extends Component {
   render () {
     const { freelances } = this.state;
 
-    
-
     return (
       <div className='Listing'>
-       
-            <ul className='everyFreelanceCards'>
-              <li>
-                {freelances.map(freelance => <Freelance key={generateKey(freelance)} id={freelance.id} firstname={freelance.firstname} lastname={freelance.lastname} urlPhoto={freelance.url_photo} job_title={freelance.job_title} />)}
-              </li>
-            </ul>
+
+        <ul className='everyFreelanceCards'>
+          <li>
+            {freelances.map(freelance => <Freelance key={generateKey(freelance)} id={freelance.id} firstname={freelance.firstname} lastname={freelance.lastname} urlPhoto={freelance.url_photo} job_title={freelance.job_title} />)}
+          </li>
+        </ul>
         <ScrollButton />
+        <BasicPagination />
       </div>
     );
   }
