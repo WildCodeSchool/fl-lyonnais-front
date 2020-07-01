a36a8ea414b024436538ed39c7d060866441cf5d
Armin <arminit3@gmail.com>
2020-07-01 02:42:50 +0200

    update detail references

--- src/components/DetailReferences.js
+++ src/components/DetailReferences.js
@@ -6,7 +6,7 @@ function DetailReferences ({ freelance }) {
     <div>
       <h3 className='detailh2'>Mes Références</h3>
       <div className='carousel'>
-        {freelance.references.map(reference => {
+        {references.map(reference => {
           return (
             <div className='reference' key={reference.id}>
               <img src={reference.image} alt={reference.name} />
@@ -20,3 +20,36 @@ function DetailReferences ({ freelance }) {
 }
 
 export default DetailReferences;
+
+const references = [
+  {
+    name: 'Redbull1',
+    image: 'https://enseignestickers.com/wp-content/uploads/2017/11/red-bull-1.jpg',
+    link: 'https://www.redbull.com/fr-fr/'
+  },
+  {
+    name: 'Redbull2',
+    image: 'https://enseignestickers.com/wp-content/uploads/2017/11/red-bull-1.jpg',
+    link: 'https://www.redbull.com/fr-fr/'
+  },
+  {
+    name: 'Redbull3',
+    image: 'https://enseignestickers.com/wp-content/uploads/2017/11/red-bull-1.jpg',
+    link: 'https://www.redbull.com/fr-fr/'
+  },
+  {
+    name: 'Redbull4',
+    image: 'https://enseignestickers.com/wp-content/uploads/2017/11/red-bull-1.jpg',
+    link: 'https://www.redbull.com/fr-fr/'
+  },
+  {
+    name: 'Redbull5',
+    image: 'https://enseignestickers.com/wp-content/uploads/2017/11/red-bull-1.jpg',
+    link: 'https://www.redbull.com/fr-fr/'
+  },
+  {
+    name: 'Redbull6',
+    image: 'https://enseignestickers.com/wp-content/uploads/2017/11/red-bull-1.jpg',
+    link: 'https://www.redbull.com/fr-fr/'
+  }
+];
