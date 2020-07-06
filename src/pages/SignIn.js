df505973107be9eb8351107f12422e86dfe15114
Armin <arminit3@gmail.com>
2020-07-06 17:40:00 +0200

    refactoring

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,18 +1,23 @@
 import React, { useState, useContext } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory, useLocation, useParams, Link } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import API from '../API';
 import AuthContext from '../components/AuthContext';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import decode from 'jwt-decode';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -35,6 +40,20 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SignIn (props) {
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+  // console.log(props.location.search);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const { saveToken } = useContext(AuthContext);
   const history = useHistory();
   const classes = useStyles();
@@ -45,23 +64,120 @@ export default function SignIn (props) {
     event.preventDefault();
     const payload = { email, password };
     API.post('/users/connexion', payload).then((res) => {
-      history.push('/');
-      alert('Vous etes connecté à votre compte et allez être redirigé vers votre page de détail');
+      const decodedToken = decode(res.data.token);
+      history.push(`/detail/${decodedToken.id}`);
+      console.log(res.data);
+      // res.data.token;
       saveToken(res.data.token);
     })
-      .catch(err => alert('Les identifiants renseignés sont incorrects.'));
+      .catch(err => {
+        console.error(err);
+        handleClickOpen();
+      });
   };
+
+  // Traitement du formulaire de demande de renvoi de l'email de validation
+  const handleSubmitValidation = (event) => {
+    event.preventDefault();
+    const payload = { email };
+    const url = process.env.REACT_APP_API_URL + '/users/renvoi_email_validation?email=' + email;
+    console.log('url = ', url);
+    axios.post(url, payload)
+      .then((res) => {
+        console.log(payload);
+      })
+      .catch(err => {
+        handleClickOpen();
+      });
+  };
+
+  // Récupération du paramètre passé dans l'url (connexion?statut=xxx)
+  const location = useLocation();
+  const search = location.search;
+  const status = search.toString().substr(8); // ne garde que le paramètre
+  const validation = {
+    validated: '', // statut=validated
+    revalidation: '', // statut=revalidation
+    delayExceeded: '', // statut=delay_exceeded
+    wrongKey: '', // statut=wrong_key
+    displayForm: '' // en fonction de l'info de validation, il se peut que le formulaire n'ai aucune utilité et se trouve don caché
+  };
+  let newFreelance = '';
+  status ? newFreelance = '' : newFreelance = 'none';
+  status === 'validated' ? validation.validated = '' : validation.validated = 'none';
+  status === 'revalidation' ? validation.revalidation = '' : validation.revalidation = 'none';
+  if (status === 'delay_exceeded') {
+    validation.delayExceeded = '';
+    validation.displayForm = 'none';
+  } else {
+    validation.delayExceeded = 'none';
+  }
+  if (status === 'wrong_key') {
+    validation.wrongKey = '';
+    validation.displayForm = 'none';
+  } else {
+    validation.wrongKey = 'none';
+  }
 
   return (
     <div>
-      <Container component='main' maxWidth='xs'>
+      <div style={{ display: `${newFreelance}` }} className='alert-freelance-validation'>
+        <h2>
+        Information nouveau freelance
+        </h2>
+        <div style={{ display: `${validation.validated}` }} className='validated'>
+          <p>Féliciation, tu peux maintenant te connecter à ton compte.</p>
+          <p>N'oublie pas de compléter ta page personnelle afin d'être plus visible.</p>
+        </div>
+        <div style={{ display: `${validation.revalidation}` }} className='revalidation'>
+          <p>Il semble que tu ais cliqué plusieurs fois de suite sur le lien pour valider ton adresse email.</p>
+          <p>Pas de souci, tu peux te connecter à ton compte en utilisant le formulaire ci-dessous.</p>
+          <p>N'oublie pas d'archiver ou d'effacer l'email...</p>
+        </div>
+        <div style={{ display: `${validation.delayExceeded}` }} className='delay-over'>
+          <p>Oups ! le délai de validation de 2 jours est dépassé !</p>
+          <p>Pas de souci, en renseignant l'adresse email qui t'a servi pour créer ton compte et en cliquant sur le lien ci-dessous, tu vas recevoir un nouveau message auquel tu devras répondre dans le deux jours !</p>
+          <form className={classes.form} noValidate onSubmit={handleSubmitValidation}>
+            <div>
+              <TextField
+                variant='outlined'
+                margin='normal'
+                required
+                id='email'
+                label='Adresse email'
+                name='email'
+                autoComplete='email'
+                autoFocus
+                value={email}
+                onChange={(e) => { setEmail(e.target.value); }}
+              />
+            </div>
+            <div>
+              <Button
+                type='submit'
+                variant='contained'
+                color='primary'
+                className={classes.submit}
+                style={{ backgroundColor: 'var(--red)' }}
+              >
+                Envoyer de nouveau l'email de validation
+              </Button>
+            </div>
+          </form>
+        </div>
+        <div style={{ display: `${validation.wrongKey}` }} className='wrong_key'>
+          <p>Il semble qu'il y ait eu un petit souci lors de la validation.</p>
+          <p>Tente de nouveau en cliquant sur le lien que tu as reçu par email en prenant garde à ne pas le modifier.</p>
+        </div>
+      </div>
+      <Container style={{ display: `${validation.displayForm}` }} component='main' maxWidth='xs'>
         <CssBaseline />
         <div className={classes.paper}>
           <Avatar style={{ backgroundColor: 'var(--red)' }} className={classes.avatar}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component='h1' variant='h5'>
-          Connexion
+            Connexion
           </Typography>
           <form className={classes.form} noValidate onSubmit={handleSubmit}>
             <TextField
@@ -70,7 +186,7 @@ export default function SignIn (props) {
               required
               fullWidth
               id='email'
-              label='Email Address'
+              label='Adresse email'
               name='email'
               autoComplete='email'
               autoFocus
@@ -90,10 +206,6 @@ export default function SignIn (props) {
               vlaue={password}
               onChange={(e) => { setPassword(e.target.value); }}
             />
-            <FormControlLabel
-              control={<Checkbox value='remember' color='primary' />}
-              label='Se souvenir de moi'
-            />
             <Button
               type='submit'
               fullWidth
@@ -102,14 +214,14 @@ export default function SignIn (props) {
               className={classes.submit}
               style={{ backgroundColor: 'var(--red)' }}
             >
-            Se connecter
+              Se connecter
             </Button>
             <Grid container>
-              <Grid item xs>
+              {/* <Grid item xs>
                 <Link to='#' variant='body2'>
                   Mot de passe oublié?
                 </Link>
-              </Grid>
+              </Grid> */}
               <Grid item>
                 <Link to='/inscription' variant='body2'>
                   {"Vous n'avez pas de compte? S'inscrire"}
@@ -118,6 +230,28 @@ export default function SignIn (props) {
             </Grid>
           </form>
         </div>
+        <Dialog
+          fullScreen={fullScreen}
+          open={open}
+          onClose={handleClose}
+          aria-labelledby='responsive-dialog-title'
+        >
+          <DialogTitle id='responsive-dialog-title'>Problème de connexion</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Les identifiants renseignés sont incorrects.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            {/* <Button autoFocus onClick={handleClose} color="primary">
+              Disagree
+          </Button> */}
+            <Button onClick={handleClose} color='primary' autoFocus>
+              Fermer
+            </Button>
+          </DialogActions>
+        </Dialog>
+
       </Container>
     </div>
   );
