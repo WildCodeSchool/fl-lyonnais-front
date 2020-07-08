9e18ca5c3a55daf220cd62a22aaf263da13a0cac
Armin <arminit3@gmail.com>
2020-07-08 15:46:42 +0200

    update mentions légales

--- src/pages/LegalDisclaimer.js
+++ src/pages/LegalDisclaimer.js
@@ -6,20 +6,19 @@ function Generic () {
     <div className='Generic'>
       <h1>Mentions légales</h1>
       <section className='section'>
-        <p>Responsable du site : Pierre Ammeloot</p>
-        <h3>Dénommination :</h3>
+        <h2>Dénommination :</h2>
         <p>Freelances Lyonnais</p>
-        <h3>Siège Social :</h3>
+        <h2>Siège Social :</h2>
         <p>18-20 rue Tronchet 69006 Lyon</p>
         <p>Immatriculée au RCS de LYON : N° 880 240 130 00018</p> 
         <p>Numéro individuel d'identification fiscale : FR65880240130</p>
-        <h3>Adresse de courrier électronique et numéro de téléphone:</h3>
-        <p>Pierre Ammeloot,pierre@webanyone.fr</p>
-        <p>+33 6 02 12 06 33</p>
-        <h3>Mentions complémentaires :</h3>
-        <p>Hébergeur du site internet : OVH, 2 rue Kellermann, 59100 Roubaix, France</p>
-        <h3>Directeur de la publication :</h3>
+        <h2>Adresse de courrier électronique et numéro de téléphone :</h2>
         <p>Pierre Ammeloot</p>
+        <p>pierre@webanyone.fr</p>
+        <p>+33 6 02 12 06 33</p>
+        <h2>Mentions complémentaires :</h2>
+        <p>Hébergeur du site internet : OVH, 2 rue Kellermann, 59100 Roubaix, France</p>
+        <p>Directeur de la publication : Pierre Ammeloot</p>
       </section>
     </div>
   );
