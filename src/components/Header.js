04686f3559b15a3021fa14358fda117946be0078
Pascal-Pereira <pereira.pascal@gmail.com>
2020-07-13 16:54:48 +0200

    boutons connexion, deconnexion rendus en fonction de la connexion de l'user

--- src/components/Header.js
+++ src/components/Header.js
@@ -62,6 +62,8 @@ export default function PrimarySearchAppBar (props) {
   const classes = useStyles();
   const setTokenInLocalStorage = useContext(AuthContext).setToken;
   const setUserInLocalStorage = useContext(AuthContext).saveUser;
+  const isConnected = !!useContext(AuthContext).token;
+  const { user } = useContext(AuthContext);
 
   const handleLogout = () => {
     setUserInLocalStorage('{}');
@@ -89,9 +91,16 @@ export default function PrimarySearchAppBar (props) {
           </div>
           <div className={classes.grow} />
           <div>
-            <Button color='inherit'><Link style={{ textDecoration: 'none', color: 'var(--white)' }} to='/inscription'>Inscription</Link></Button>
-            <Button color='inherit'><Link style={{ textDecoration: 'none', color: 'var(--white)' }} to='/connexion'>Connexion</Link></Button>
-            <Button color='inherit'><Link onClick={handleLogout} style={{ textDecoration: 'none', color: 'var(--white)' }} to='/connexion'>Déconnexion</Link></Button>
+            { isConnected && user && user.freelance_id && <Button color='inherit'><Link style={{ textDecoration: 'none', color: 'var(--white)' }} to={`/detail/${user.freelance_id}`}>Mon Compte</Link></Button>}
+
+            {isConnected && <Button color='inherit'><Link onClick={handleLogout} style={{ textDecoration: 'none', color: 'var(--white)' }} to='/connexion'>Déconnexion</Link></Button>}
+
+            {!isConnected && 
+            <>
+              <Button color='inherit'><Link style={{ textDecoration: 'none', color: 'var(--white)' }} to='/inscription'>Inscription</Link></Button>
+              <Button color='inherit'><Link style={{ textDecoration: 'none', color: 'var(--white)' }} to='/connexion'>Connexion</Link></Button>
+            </>
+            }
           </div>
         </Toolbar>
       </AppBar>
