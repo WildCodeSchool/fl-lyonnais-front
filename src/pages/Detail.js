d0742d6c05291f7242fcd0da482c048e42848854
Pascal-Pereira <pereira.pascal@gmail.com>
2020-07-13 17:10:18 +0200

    us6 avec correction PR

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component,useState, useEffect, useContext } from 'react';
+import {useParams} from 'react-router-dom';
+
 import DetailBio from '../components/Detail/DetailBio';
 import DetailReferences from '../components/Detail/DetailReferences';
 import DetailSkills from '../components/Detail/DetailSkills';
@@ -7,50 +9,42 @@ import API from '../API';
 import Buttons from '../components/Buttons';
 import AuthContext from '../components/AuthContext';
 
-class Detail extends Component {
-  static contextType = AuthContext
-  constructor (props) {
-    super(props);
-    this.state = {
-      freelances: [],
-      tags: [],
-      references: [],
-      id: this.props.match.params.id
-    };
-  }
 
-  componentDidMount () {
-    API.get('/freelances/' + this.state.id)
-      .then(response => response.data)
-      .then(data => {
-        this.setState({
-          freelances: data.freelance,
-          tags: data.tags,
-          references: data.references,
-          is_active: data.freelance.is_active
-        });
-      });
-  }
+function Detail (props){
 
-  render () {
-    const { freelances, tags, references, is_active } = this.state;
-    const { user } = this.context
+  const [freelance,setFreelance] = useState({})
+  const [tags,setTags] = useState([])
+  const [references,setReferences] = useState([])
+  const [is_active,setIsActive] = useState()
+  const  { id }= useParams()
 
-    return (
-      <div>
-        {(is_active === 1 || is_active === 0) && (user && user.freelance_id == this.state.id) && <Buttons id={this.state.id} is_active={is_active}/>}
-        <h1>Page détail freelance</h1>
-        <div className='Detail'>
-          <DetailBio freelances={freelances} />
-          <DetailReferences references={references} />
-          <div className='responsiveSkillsContact'>
-            <DetailSkills tags={tags} freelances={freelances} />
-            <DetailContact freelances={freelances} />
-          </div>
-        </div>
+  useEffect( () => {
+        API.get('/freelances/' + id)
+        .then(response => response.data)
+        .then(data => {
+          setFreelance(data.freelance)
+          setTags(data.tags);
+          setReferences(data.references);
+          setIsActive(data.freelance.is_active)
+        })},[id]);
+
+        const {user} = useContext(AuthContext)
+  return(
+
+    <div>
+    {(is_active === 1 || is_active === 0) && (user && user.freelance_id == id) && <Buttons id={id} is_active={is_active}/>}
+    <h1>Page détail freelance</h1>
+    <div className='Detail'>
+      <DetailBio freelances={freelance} />
+      <DetailReferences references={references} />
+      <div className='responsiveSkillsContact'>
+        <DetailSkills tags={tags} freelances={freelance} />
+        <DetailContact freelances={freelance} />
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+      
+  );
+};
 
 export default Detail;
