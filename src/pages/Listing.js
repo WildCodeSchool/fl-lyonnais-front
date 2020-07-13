bbbe09ebc5662b64d4d196e66962847fbdb5c82e
Armin <arminit3@gmail.com>
2020-07-13 17:44:03 +0200

    update

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -10,7 +10,7 @@ const Listing = () => {
   const [totalFreelances, setTotalFreelances] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [freelancesPerPage] = useState(15);
+  const [freelancesPerPage] = useState(20);
   const paginate = pageNumber => setCurrentPage(pageNumber);
   const pageNumbers = [];
 
