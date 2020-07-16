c19b005e793b9b37b1d8add730e6d1e6fdf4d591
ikramb-ctrl <b_zakia@homail.com>
2020-07-16 20:04:49 +0200

    carousel modif

--- src/components/Detail/Carousel.js
+++ src/components/Detail/Carousel.js
@@ -83,7 +83,24 @@ function SwipeableTextMobileStepper(props) {
             </div>
           ))}
         </AutoPlaySwipeableViews>
-        
+        <MobileStepper
+          steps={maxSteps}
+          position="static"
+          variant="text"
+          activeStep={activeStep}
+          nextButton={
+            <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+              
+              {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </Button>
+          }
+          backButton={
+            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+              {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+              
+            </Button>
+          }
+        />
       </div>
     </div>
     
