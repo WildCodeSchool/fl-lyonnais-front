5f00eb460ff17512d0c78ba1f1216d595d5764fb
ikramb-ctrl <b_zakia@homail.com>
2020-07-16 18:45:07 +0200

    suppression back et next carrousel reference

--- src/components/Detail/Carousel.js
+++ src/components/Detail/Carousel.js
@@ -83,24 +83,7 @@ function SwipeableTextMobileStepper(props) {
             </div>
           ))}
         </AutoPlaySwipeableViews>
-        <MobileStepper
-          steps={maxSteps}
-          position="static"
-          variant="text"
-          activeStep={activeStep}
-          nextButton={
-            <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
-              Next
-              {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </Button>
-          }
-          backButton={
-            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-              {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-              Back
-            </Button>
-          }
-        />
+        
       </div>
     </div>
     
