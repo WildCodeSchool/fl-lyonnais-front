eca914a264bbf1a7d34dc61a48f85e51de4d998d
Armin <arminit3@gmail.com>
2020-07-17 02:55:49 +0200

    update upload button

--- src/components/FormEdition/UploadButtons.js
+++ src/components/FormEdition/UploadButtons.js
@@ -51,7 +51,7 @@ export default function UploadButtons() {
     <div className={classes.root}>
       <input accept='image/*' className={classes.input} id='contained-button-file' multiple type='file' onChange={handlePictureProfileUpload} />
       <label htmlFor='contained-button-file'>
-        <Button style={{ backgroundColor: 'var(--red)' }} variant='contained' color='primary' component='span' className={classes.button} startIcon={<CloudUploadIcon />}>Télécharger</Button>
+        <Button style={{ backgroundColor: 'var(--green)' }} variant='contained' color='primary' component='span' className={classes.button} startIcon={<CloudUploadIcon />}>Photo de profil</Button>
       </label>
     </div>
   );
