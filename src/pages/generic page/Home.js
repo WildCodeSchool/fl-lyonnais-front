2df7125bd1931edae75ce41e24700616314859ab
Christophe <porvooc@gmail.com>
2020-07-27 10:50:10 +0200

    fix - ajout bouton lien vers l'inscription après le dernier paragraphe, home.js

--- src/pages/generic page/Home.js
+++ src/pages/generic page/Home.js
@@ -22,7 +22,7 @@ function Home () {
         <div className='paragraphe'>
           <h3>Qui se cache derrière Les Freelances Lyonnais ? </h3>
           <p style={{ marginTop: '16px', marginBottom: '0' }}>Si vous êtes arrivé.e.s sur cette page, c’est que vous en connaissez au moins un.e :) Vous avez apprécié la qualité de son travail, sa proximité géographique, et sa disponibilité. Imaginez maintenant qu’on vous dise qu’on connaît 1800 autres professionnel.le.s comme ça ?</p>
-          <Button variant="contained" color='primary' style={{ backgroundColor: '#1E2CFB' }}>
+          <Button variant='contained' color='primary' style={{ backgroundColor: '#1E2CFB' }}>
             <Link style={{ color: 'white' }} to='/liste_freelance/'>Les Freelances Lyonnais</Link>
           </Button>
         </div>
@@ -34,7 +34,10 @@ function Home () {
 
         <div className='paragraphe'>
           <h3>Un arsenal de compétences dans lequel piocher </h3>
-          <p>L’annuaire des Freelances, c’est un peu comme faire son marché ou visiter son magasin de quartier : vous appuyez le commerce local et vous avez l’avantage d’une offre sur mesure, dans votre langue, voire votre quartier ! Ce Freelance bénéficie de l’appui de son réseau pour aller chercher une compétence additionnelle dont votre projet aura besoin, soumettre à la sagesse de la foule connaisseuse les questions pointues ou encore vous référer un autre prestataire de confiance.</p>
+          <p style={{ marginTop: '16px', marginBottom: '0' }}>L’annuaire des Freelances, c’est un peu comme faire son marché ou visiter son magasin de quartier : vous appuyez le commerce local et vous avez l’avantage d’une offre sur mesure, dans votre langue, voire votre quartier ! Ce Freelance bénéficie de l’appui de son réseau pour aller chercher une compétence additionnelle dont votre projet aura besoin, soumettre à la sagesse de la foule connaisseuse les questions pointues ou encore vous référer un autre prestataire de confiance.</p>
+          <Button variant='contained' color='primary' style={{ backgroundColor: '#1E2CFB' }}>
+            <Link style={{ color: 'white' }} to='/inscription/'>Je créé ma fiche freelance gratuitement</Link>
+          </Button>
         </div>
       </main>
     </div>
