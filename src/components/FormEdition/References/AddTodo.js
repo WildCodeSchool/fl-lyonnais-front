c9b3ffced05d4736fb30536df63f066d237baccc
Armin <arminit3@gmail.com>
2020-07-28 11:24:20 +0200

    fix

--- src/components/FormEdition/References/AddTodo.js
+++ src/components/FormEdition/References/AddTodo.js
@@ -3,7 +3,14 @@ import { TextField, Paper, Button, Grid } from '@material-ui/core';
 import EditionContext from '../EditionContext';
 
 const AddTodo = (props) => {
-  const { addReference } = useContext(EditionContext);
+  const { addReference, newReferenceName, setNewReferenceName } = useContext(EditionContext);
+
+  const handleInputKeyPress = (e) => {
+    const keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == '13') {
+      addReference();
+    }
+  };
 
   return (
     <Paper style={{ margin: 16, padding: 16 }}>
@@ -11,9 +18,9 @@ const AddTodo = (props) => {
         <Grid xs={9} md={11} item style={{ paddingRight: 16, maxWidth: 'calc(100% - 90px)' }}>
           <TextField
             placeholder='Nom du projet'
-            value={props.inputValueName}
-            onChange={props.onInputChangeName}
-            onKeyPress={props.onInputKeyPressName}
+            value={newReferenceName}
+            onChange={(e) => setNewReferenceName(e.target.value)}
+            onKeyPress={handleInputKeyPress}
             fullWidth
           />
         </Grid>
